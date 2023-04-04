a0e5440ba14aed5bf6f00762f9f1a6c0e6b0893a
Yukichi <naginoyukichi@gmail.com>
2023-04-04 18:38:32 +0900

    pandas

--- main.py
+++ main.py
@@ -4,3 +4,32 @@ import pandas as pd
 
 st.title('Streamlit 超入門')
 st.write('DataFrame')
+
+df = pd.DataFrame(
+    np.random.rand(20, 3),
+    columns=['a', 'b', 'c']
+)
+st.bar_chart(df)
+
+df = pd.DataFrame({
+    '1列目': [1, 2, 3, 4],
+    '2列目': [10, 20, 30, 40]
+})
+
+st.write(df)
+# widthとheightが選択できる
+st.dataframe(df.style.highlight_max(axis=0), width=1000, height=500)
+# 静的なテーブル
+st.table(df.style.highlight_max(axis=0))
+
+"""
+# 章
+## 節
+### 項
+
+```python
+import streamlit as st
+import numpy as np
+import pandas as pd
+```
+"""
