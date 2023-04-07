1a256c858e56af17df81bbddd004ba133f9a8c2b
Yukichi <naginoyukichi@gmail.com>
2023-04-07 09:45:14 +0900

    2

--- main.py
+++ main.py
@@ -5,22 +5,43 @@ import pandas as pd
 st.title('Streamlit 超入門')
 st.write('DataFrame')
 
-df = pd.DataFrame(
-    np.random.rand(20, 3),
-    columns=['a', 'b', 'c']
-)
-st.bar_chart(df)
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右カラム')
+if button:
+  right_column.write('ここは右カラム')
 
-df = pd.DataFrame({
-    '1列目': [1, 2, 3, 4],
-    '2列目': [10, 20, 30, 40]
-})
+expander = st.beta_expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
 
-st.write(df)
-# widthとheightが選択できる
-st.dataframe(df.style.highlight_max(axis=0), width=1000, height=500)
-# 静的なテーブル
-st.table(df.style.highlight_max(axis=0))
+text = st.text_input('あなたの趣味を教えてください')
+'あなたの趣味:', text
+
+condition = st.sidebar.slider('あなたの今の調子は', 0, 100, 50)
+'コンディション：', condition
+
+
+# df = pd.DataFrame(
+#     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
+#     columns=['lat', 'lon']
+# )
+# st.map(df)
+
+# df = pd.DataFrame(
+#     np.random.rand(20, 3),
+#     columns=['a', 'b', 'c']
+# )
+# st.bar_chart(df)
+
+# df = pd.DataFrame({
+#     '1列目': [1, 2, 3, 4],
+#     '2列目': [10, 20, 30, 40]
+# })
+
+# st.write(df)
+# # widthとheightが選択できる
+# st.dataframe(df.style.highlight_max(axis=0), width=1000, height=500)
+# # 静的なテーブル
+# st.table(df.style.highlight_max(axis=0))
 
 """
 # 章
